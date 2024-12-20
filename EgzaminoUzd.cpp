9c1bfe64e38a45e51eb6d3c95e8dd2049b296a63
Dominyka Abaravičiūtė <dominyka.abaraviciute@mif.stud.vu.lt>
2024-12-20 08:30:17 +0200

    Eilutės, kuriose pasikartoja žodžiai.

--- EgzaminoUzd.cpp
+++ EgzaminoUzd.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <sstream>
 #include <algorithm>
+#include <set>
 
 std::wstring isvalytiZodi(const std::wstring& zodis) {
     std::wstring svarusZodis = zodis;
@@ -24,7 +25,7 @@ void toLower(std::wstring& str) {
     std::transform(str.begin(), str.end(), str.begin(), ::towlower);
 }
 
-void skaitytiFaila(const std::string& failoPavadinimas, std::wstring& platusTurinys) {
+void skaitytiFaila(const std::string& failoPavadinimas, std::map<std::wstring, int>& zodziuDaznis, std::map<std::wstring, std::set<int>>& zodziuEilutes) {
     std::setlocale(LC_ALL, "lt_LT.UTF-8");
 
     std::ifstream failas(failoPavadinimas, std::ios::binary);
@@ -33,9 +34,12 @@ void skaitytiFaila(const std::string& failoPavadinimas, std::wstring& platusTuri
         std::cerr << "Nepavyko atidaryti failo: " << failoPavadinimas << std::endl;
         return;
     }
+
     std::wstring_convert<std::codecvt_utf8<wchar_t>> keitiklis;
 
     std::string eilute;
+    int eilutesNumeris = 1;
+
     while (std::getline(failas, eilute)) {
         std::wstring platiEilute = keitiklis.from_bytes(eilute);
         toLower(platiEilute);
@@ -45,25 +49,31 @@ void skaitytiFaila(const std::string& failoPavadinimas, std::wstring& platusTuri
         while (srautas >> zodis) {
             std::wstring svarusZodis = isvalytiZodi(zodis);
             if (!svarusZodis.empty()) {
-                platusTurinys += svarusZodis + L" ";
+                ++zodziuDaznis[svarusZodis];
+                zodziuEilutes[svarusZodis].insert(eilutesNumeris);
             }
         }
+        eilutesNumeris++;
     }
 
     failas.close();
 }
 
 
-void rasytiFaila(const std::map<std::wstring, int>& zodziuDaznis, const std::string& isvestiesFailoPavadinimas) {
-    std::ofstream isvestiesFailas(isvestiesFailoPavadinimas, std::ios::binary);
+void rasytiFaila(const std::map<std::wstring, int>& zodziuDaznis, const std::map<std::wstring, std::set<int>>& zodziuEilutes, const std::string& isvestiesFailoPavadinimas) {
+    std::ofstream isvestiesFailas(isvestiesFailoPavadinimas, std::ios::out);
 
     if (!isvestiesFailas.is_open()) {
         std::cerr << "Nepavyko atidaryti failo: " << isvestiesFailoPavadinimas << std::endl;
         return;
     }
 
+    isvestiesFailas.imbue(std::locale("lt_LT.UTF-8"));
+
     std::wstring_convert<std::codecvt_utf8<wchar_t>> keitiklis;
 
+    std::string skiltis1 = keitiklis.to_bytes(L"Daugiau nei kartà tekste pasikartojantys þodþiai:\n");
+    isvestiesFailas << skiltis1;
     for (const auto& zodis : zodziuDaznis) {
         if (zodis.second > 1) {
             std::string eilute = keitiklis.to_bytes(zodis.first + L": " + std::to_wstring(zodis.second) + L"\n");
@@ -72,19 +82,21 @@ void rasytiFaila(const std::map<std::wstring, int>& zodziuDaznis, const std::str
 
     }
 
-    isvestiesFailas.close();
-}
-
-void skaiciuotiZodzius(const std::wstring& turinys, std::map<std::wstring, int>& zodziuDaznis) {
-    std::wstringstream srautas(turinys);
-    std::wstring zodis;
-
-    while (srautas >> zodis) {
-        std::wstring svarusZodis = isvalytiZodi(zodis);
-        if (!svarusZodis.empty()) {
-            ++zodziuDaznis[zodis];
-        }     
+    std::string skiltis2 = keitiklis.to_bytes(L"\nCross-reference lentelë:\n");
+    isvestiesFailas << skiltis2;
+    for (const auto& zodis : zodziuDaznis) {
+        if (zodis.second > 1) {
+            std::string eilute = keitiklis.to_bytes(zodis.first + L": ");
+            for (const auto& eiluteNr : zodziuEilutes.at(zodis.first)) {
+                eilute += std::to_string(eiluteNr) + ", ";
+            }
+            eilute = eilute.substr(0, eilute.length() - 2);
+            eilute += "\n";
+            isvestiesFailas << eilute;
+        }
     }
+
+    isvestiesFailas.close();
 }
 
 int main() {
@@ -93,13 +105,12 @@ int main() {
 
     std::setlocale(LC_ALL, "lt_LT.UTF-8");
 
-    std::wstring platusTurinys;
-    skaitytiFaila(failoPavadinimas, platusTurinys);
-
     std::map<std::wstring, int> zodziuDaznis;
-    skaiciuotiZodzius(platusTurinys, zodziuDaznis);
+    std::map<std::wstring, std::set<int>> zodziuEilutes;
 
-    rasytiFaila(zodziuDaznis, isvestiesFailoPavadinimas);
+    skaitytiFaila(failoPavadinimas, zodziuDaznis, zodziuEilutes);
+
+    rasytiFaila(zodziuDaznis, zodziuEilutes, isvestiesFailoPavadinimas);
 
     return 0;
 }
