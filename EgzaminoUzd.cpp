97a22fa5b62b1b34b938b93e26cf4923aac85285
Dominyka Abaravičiūtė <dominyka.abaraviciute@mif.stud.vu.lt>
2024-12-20 06:49:39 +0200

    Švarūs žodžiai.

--- EgzaminoUzd.cpp
+++ EgzaminoUzd.cpp
@@ -5,6 +5,20 @@
 #include <codecvt>
 #include <map>
 #include <sstream>
+#include <algorithm>
+
+std::wstring isvalytiZodi(const std::wstring& zodis) {
+    std::wstring svarusZodis = zodis;
+
+    size_t start = svarusZodis.find_first_not_of(L"0123456789.,!?\"'()[]{}<>;:-„“”");
+    size_t end = svarusZodis.find_last_not_of(L"0123456789.,!?\"'()[]{}<>;:-„“”");
+
+    if (start == std::wstring::npos || end == std::wstring::npos) {
+        return L"";
+    }
+
+    return svarusZodis.substr(start, end - start + 1);
+}
 
 void skaitytiFaila(const std::string& failoPavadinimas, std::wstring& platusTurinys) {
     std::setlocale(LC_ALL, "lt_LT.UTF-8");
@@ -15,16 +29,25 @@ void skaitytiFaila(const std::string& failoPavadinimas, std::wstring& platusTuri
         std::cerr << "Nepavyko atidaryti failo: " << failoPavadinimas << std::endl;
         return;
     }
-
     std::wstring_convert<std::codecvt_utf8<wchar_t>> keitiklis;
 
-    std::string failoTurinys((std::istreambuf_iterator<char>(failas)),
-        std::istreambuf_iterator<char>());
+    std::string eilute;
+    while (std::getline(failas, eilute)) {
+        std::wstring platiEilute = keitiklis.from_bytes(eilute);
 
-    platusTurinys = keitiklis.from_bytes(failoTurinys);
+        std::wstringstream srautas(platiEilute);
+        std::wstring zodis;
+        while (srautas >> zodis) {
+            std::wstring svarusZodis = isvalytiZodi(zodis);
+            if (!svarusZodis.empty()) {
+                platusTurinys += svarusZodis + L" ";
+            }
+        }
+    }
 
     failas.close();
 }
+
 
 void rasytiFaila(const std::map<std::wstring, int>& zodziuDaznis, const std::string& isvestiesFailoPavadinimas) {
     std::ofstream isvestiesFailas(isvestiesFailoPavadinimas, std::ios::binary);
@@ -49,7 +72,10 @@ void skaiciuotiZodzius(const std::wstring& turinys, std::map<std::wstring, int>&
     std::wstring zodis;
 
     while (srautas >> zodis) {
-        ++zodziuDaznis[zodis];
+        std::wstring svarusZodis = isvalytiZodi(zodis);
+        if (!svarusZodis.empty()) {
+            ++zodziuDaznis[zodis];
+        }     
     }
 }
 
